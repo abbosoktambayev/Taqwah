a46da0036f03655c9e0852d4d8a9fa5ff450d4ae
abbosoktambayev <oktambayevabbos1@gmail.com>
2025-11-20 12:34:03 +0500

    Initial Commit

---
+++ Taqwah/TaqwahApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaqwahApp.swift
+//  Taqwah
+//
+//  Created by Abbos Oktambayev on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaqwahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
